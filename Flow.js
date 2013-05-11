34d6e68ab3de64eab5d31e5099cf3141c922161a
Brian Hebert <mrabear@gmail.com>
2013-05-11 17:51:12 -0400

    Added the game canvas and JS hooks

---
+++ Flow.js
@@ -0,0 +1,13 @@
+//Wait for DOM to load before starting a game
+$(window).ready(function(){ 
+	init(); 
+});
+
+//Create images
+Image testImage = new Image();
+
+
+function init(){
+	//testImage.src = balls to your face;
+}
+
